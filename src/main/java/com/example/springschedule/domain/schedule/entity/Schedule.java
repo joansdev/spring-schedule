e76b80eeebc567431f805c3e47bf5f0be76a6236
joansdev <joans.dev@gmail.com>
2025-06-12 01:13:52 +0900

    feat: Schedule 엔티티 + Repository 생성

--- src/main/java/com/example/springschedule/domain/schedule/entity/Schedule.java
+++ src/main/java/com/example/springschedule/domain/schedule/entity/Schedule.java
@@ -1,8 +1,31 @@
 package com.example.springschedule.domain.schedule.entity;
 
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Schedule {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private
+
+    @Column(nullable = false)
+    private String name; // 작성자 이름
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 
 }
